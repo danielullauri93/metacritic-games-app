1cd1abef8608b54411183ca812eeb7ac8b03e87e
Daniel-Remote <dangerrestrictedzone@gmail.com>
2024-07-18 17:19:58 -0500

    istalacion de eslint

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+const icon = require('./assets/icon.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source={icon} style={{width: 100, height: 100}}/>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#09f",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
