9c6d3fa13f116dec502049d3a4ae67c700e35fc4
daniel-remote <dangerrestrictedzone@gmail.com>
2024-07-23 10:02:11 -0500

    FlatList

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,16 +1,10 @@
 import { useEffect, useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  Image,
-  ActivityIndicator,
-} from "react-native";
+import { FlatList, View, ActivityIndicator } from "react-native";
 import { getLatestGames } from "../lib/metacritic";
 // import Constants from "expo-constants";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { GameCard } from "./GameCard";
+import { Logo } from "../components/Logo";
 export function Main() {
   const [games, setGames] = useState([]);
   const insets = useSafeAreaInsets(); // Usamos el hook para obtener las insets de la pantalla.
@@ -22,15 +16,24 @@ export function Main() {
   }, []);
   return (
     <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
+      <View style={{ marginBottom: 20 }}>
+        <Logo />
+      </View>
       {games.length === 0 ? (
         <ActivityIndicator size="large" color="#fff" /> // Si no hay juegos o carga los juegos , mostramos un spinner
       ) : (
-        <ScrollView>
+        <FlatList
+          data={games}
+          keyExtractor={(game) => game.slug}
+          renderItem={({ item }) => <GameCard game={item} />}
+        >
           {games.map((game) => (
             <GameCard key={game.slug} game={game} />
           ))}
-        </ScrollView>
+        </FlatList>
       )}
     </View>
   );
 }
+
+/* En el style del primer View usamos el hook que hicimos con safeareainsets para que tenga una separacion por ambas partes */
