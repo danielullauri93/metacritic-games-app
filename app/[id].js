e6120379c300fcc3e925f40d3f66679c48be2258
danielullauri93 <ullauriochoadaniel@gmail.com>
2024-07-29 16:41:46 -0500

    Creacion del detalle del juego [id].js

--- app/[id].js
+++ app/[id].js
@@ -1,13 +1,15 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { Link } from "expo-router";
+import { Link, useLocalSearchParams } from "expo-router";
 
 export default function Detail() {
+  const { id } = useLocalSearchParams();
+
   return (
     <View className="flex-1 justify-center items-center">
       <View>
         <Text className="text-white font-bold mb-8 text-2xl">
-          Detalle del juego
+          Detalle del juego {id}
         </Text>
         <Link href="/" className="text-blue-500">
           Volver atras
